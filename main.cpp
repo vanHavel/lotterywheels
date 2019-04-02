d0fa384d560f5cd4ad5413e56e6b4fe96d1463d7
Lukas <dev.lukas.huwald@gmail.com>
2019-04-02 21:08:49 +0200

    added binomial coefficients and computation of combinations

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <bitset>
 #include <iostream>
 #include <vector>
 
@@ -15,4 +16,23 @@ std::vector<std::vector<int32_t>> computeBinomialCoefficients(std::size_t n) {
             coefficients[i][j] = coefficients[i-1][j] + coefficients[i-1][j-1];
         }
     }
+    return coefficients;
+}
+
+template <std::size_t N> std::vector<std::bitset<N>> computeAllCombinations(std::size_t k) {
+    return computeAllCombinations<N>(k, 0);
+}
+
+template <std::size_t N> std::vector<std::bitset<N>> computeAllCombinations(std::size_t k, int from) {
+    std::vector<std::bitset<N>> combinations;
+    for (int i = from; i < N; ++i) {
+        auto subCombinations = computeAllCombinations<N>(k - 1, from + 1);
+        std::transform(
+                subCombinations.begin(),
+                subCombinations.end(),
+                combinations.end(),
+                [i](auto set) { set[i] = true; return set; }
+        );
+    }
+    return combinations;
 }
