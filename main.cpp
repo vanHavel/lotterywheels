3ba57804373e7f356f86d712271bf6d2cfe51d92
Lukas <dev.lukas.huwald@gmail.com>
2019-04-04 00:59:23 +0200

    added inverse mapping

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <bitset>
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 
-std::vector<std::vector<int32_t>> computeBinomialCoefficients(std::size_t n) {
-    std::vector<std::vector<int32_t>> coefficients(n+1, std::vector<int32_t>(n+1, 0));
+std::vector<std::vector<int32_t>> computeBinomialCoefficients(int n) {
+    std::vector<std::vector<int32_t>> coefficients((std::size_t) n+1, std::vector<int32_t>((std::size_t) n+1, 0));
     for (int i = 0; i <= n; ++i) {
         coefficients[i][0] = 1;
         coefficients[i][i] = 1;
@@ -16,7 +17,7 @@ std::vector<std::vector<int32_t>> computeBinomialCoefficients(std::size_t n) {
     return coefficients;
 }
 
-template <std::size_t N> std::vector<std::bitset<N>> computeAllCombinations(std::size_t k, int from) {
+template <int N> std::vector<std::bitset<N>> computeAllCombinations(int k, int from) {
     if (k == 0) { return {std::bitset<N>()}; }
     std::vector<std::bitset<N>> combinations;
     for (int i = from; i < N - k + 1; ++i) {
@@ -31,11 +32,22 @@ template <std::size_t N> std::vector<std::bitset<N>> computeAllCombinations(std:
     return combinations;
 }
 
-template <std::size_t N> std::vector<std::bitset<N>> computeAllCombinations(std::size_t k) {
+template <int N> std::vector<std::bitset<N>> computeAllCombinations(int k) {
     return computeAllCombinations<N>(k, 0);
 }
 
+template <int N> std::unordered_map<std::bitset<N>, uint32_t> computeInverseMapping(std::vector<std::bitset<N>> vec) {
+    std::unordered_map<std::bitset<N>, uint32_t> res;
+    for (uint32_t i = 0; i < vec.size(); ++i) {
+        res[vec[i]] = i;
+    }
+    return res;
+}
+
 int main() {
-    auto combs = computeAllCombinations<49>(6, 0);
-    std::cout << combs.size();
+    auto combs = computeAllCombinations<5>(3, 0);
+    auto inv = computeInverseMapping<5>(combs);
+    for (auto iter = inv.begin(); iter != inv.end(); ++iter) {
+        std::cout << iter->first << " -> " << iter->second << "\n";
+    }
 }
